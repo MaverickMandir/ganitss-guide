626b51a53379079ee80595bb2e23ccf1f6c18e8b
Abhro Jit M <abhro.j@pm.me>
2023-06-30 16:31:55 +0530

    patch: Layout & BN, HN Support

--- components/title.js
+++ components/title.js
@@ -3,8 +3,10 @@ import React from 'react'
 export default function Title() {
   return (
     <span
-        className="mx-2 antialiased tracking-tight uppercase select-none flex text-xl md:text-2xl font-black bg-gradient-to-tl bg-clip-text text-transparent from-indigo-500 via-purple-500 to-pink-500">
-        Ganitam
+        className="mx-2 antialiased uppercase select-none flex text-2xl lg:text-3xl font-extrabold bg-gradient-to-tl bg-clip-text text-transparent from-indigo-500 via-purple-500 to-pink-500">
+        <span className="sm:hidden"> GSS </span>
+        <span className="hidden sm:inline-block lg:hidden"> Ganit SS</span>
+        <span className="hidden lg:inline-block"> Ganit Swayam Shikshak</span>
     </span>
   )
 }
