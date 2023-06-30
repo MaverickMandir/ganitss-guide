e3a41e4bfd217c388c1887ba1f022992595fe3c9
Arpan Mondal <arpan.m@pm.me>
2023-06-30 09:10:06 +0530

    patch: 1.1

---
+++ components/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <span
+      className="mx-4 antialiased uppercase select-none flex text-xl font-mono font-semibold bg-gradient-to-tl bg-clip-text text-transparent to-indigo-500 via-purple-500 from-pink-500">
+        MIT {new Date().getFullYear()} © Ganit Swayam Shikshak, Maverick Mandir.
+    </span>
+  )
+}
