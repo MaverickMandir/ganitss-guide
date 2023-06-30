a0d176246e6c6837ce6cac93a1c472e7901c55f4
Arpan Mon <arpan.m@pm.me>
2023-06-30 10:41:56 +0530

    patch: Font Style

--- components/title.js
+++ components/title.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Title() {
   return (
     <span
-        className="mx-2 antialiased tracking-tight uppercase font-mono select-none flex text-xl md:text-2xl font-black bg-gradient-to-tl bg-clip-text text-transparent from-indigo-500 via-purple-500 to-pink-500">
+        className="mx-2 antialiased tracking-tight uppercase select-none flex text-xl md:text-2xl font-black bg-gradient-to-tl bg-clip-text text-transparent from-indigo-500 via-purple-500 to-pink-500">
         Ganitam
     </span>
   )
