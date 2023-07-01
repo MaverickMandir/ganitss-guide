f137506c4b7e0f3ab459916210d08a7beb8ee83e
Arpan Mondal <arpan.m@pm.me>
2023-07-01 18:09:14 +0530

    chore: Title

--- components/logo.js
+++ components/logo.js
@@ -2,14 +2,14 @@ export default function Logo() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="48"
-      height="48"
+      width="30"
+      height="30"
       fill="none"
       viewBox="0 0 48 48"
     >
-      <path fill="#1E1E1E" d="M0 0H48V48H0z"></path>
+      <path d="M0 0H48V48H0z"></path>
       <circle cx="24" cy="24" r="24" fill="#000"></circle>
-      <path fill="url(#paint0_angular_0_1)" d="M10 10H38V38H10z"></path>
+      <path fill="#fff" d="M10 10H38V38H10z"></path>
       <path fill="#000" d="M17 17H20V38H17z"></path>
       <path
         fill="#000"
@@ -23,19 +23,6 @@ export default function Logo() {
         d="M30.414 18.899H32.414V30.899H30.414z"
         transform="rotate(-135 30.414 18.9)"
       ></path>
-      <defs>
-        <radialGradient
-          id="paint0_angular_0_1"
-          cx="0"
-          cy="0"
-          r="1"
-          gradientTransform="rotate(55.62 -8.735 28.235) scale(11.5109 12.23)"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop offset="0.531" stopColor="#E29DE3"></stop>
-          <stop offset="1" stopColor="#89CEC2"></stop>
-        </radialGradient>
-      </defs>
     </svg>
   );
 }
--- components/title.js
+++ components/title.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Logo from './logo'
 
 export default function Title() {
   return (
-    <span
-        className="mx-2 antialiased uppercase select-none flex text-2xl lg:text-3xl font-extrabold bg-gradient-to-tl bg-clip-text text-transparent from-indigo-500 via-purple-500 to-pink-500">
-        <span className="sm:hidden"> GSS </span>
-        <span className="hidden sm:inline-block lg:hidden"> Ganit SS</span>
-        <span className="hidden lg:inline-block"> Ganit Swayam Shikshak</span>
-    </span>
+    <>
+      <Logo/>
+      <span
+          className="mx-2 pl-2 antialiased uppercase select-none flex text-2xl lg:text-3xl font-extrabold bg-gradient-to-tl bg-clip-text text-transparent from-indigo-500 via-purple-500 to-pink-500">
+          <span className="md:hidden"> GSS </span>
+          <span className="hidden md:inline-block lg:hidden"> Ganit SS</span>
+          <span className="hidden lg:inline-block"> Ganit Swayam Shikshak</span>
+      </span>
+    </>
   )
 }
